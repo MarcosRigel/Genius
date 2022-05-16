c84f60b8e47cdd1865329c26363f0fccdf0cdfb0
Marcos Rigel <marcosrigel99@gmail.com>
2022-05-16 12:30:18 -0300

    terceira parte

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+//cria ordem aleatoria de cores
 let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
   order[order.length] = colorOrder;
@@ -23,6 +24,7 @@ let shuffleOrder = () => {
   }
 }
 
+// acende a proxima cor
 let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
@@ -32,3 +34,30 @@ let lightColor = (element, number) => {
       element.classList.remove('selected');
     });
 }
+
+// checa se os botoes clicados são os mesmo da ordem gerada no jogo
+let checkOrder = () =>  {
+  for(let i in clickedOrder) {
+    if(clickedOrder[i] != order[i]) {
+      lose();
+      break;
+    }
+  }
+  if(clickedOrder.length == order.length) {
+    alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+    nextLevel();
+  }
+}
+
+//função para o clique do usuario
+let click = (color) => {
+  clickedOrder[clickedOrder.length] = color;
+  createcolorElement(color).classList.add('selected');
+
+  setTimeout(() => {
+    createcolorElement(color).classList.remove('selected');
+  })
+
+  checkOrder();
+}
+
